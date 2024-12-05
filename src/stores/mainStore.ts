35da349ac77e1d245562a95e4377f319e19aca52
Dmitriy <dfajl@mail.ru>
2024-12-05 17:30:59 +0300

    Initial commit with Vue 3 + TypeScript + Pinia + Vue Router setup

---
+++ src/stores/mainStore.ts
@@ -0,0 +1,13 @@
+import { defineStore } from 'pinia';
+
+export const useMainStore = defineStore('mainStore', {
+	state: () => ({
+		count: 0,
+	}),
+	actions: {
+		increment() {
+			this.count++;
+		},
+	},
+	getters: {},
+});
