a566e35c8dfe7fb88f935b86ee4c3884ba9edad7
Dmitriy <dfajl@mail.ru>
2025-01-14 12:34:35 +0300

    Comment deleted

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import router from '@/router/router';
 import App from '@/App.vue';
-import { createToast } from '@/services/toastService.ts'; // Импортируем функцию создания тостов
+import { createToast } from '@/services/toastService.ts';
 
 const { toasts, addToast } = createToast();
 const app = createApp(App);
